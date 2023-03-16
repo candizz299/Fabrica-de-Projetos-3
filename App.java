ea8c5e619627d65451e05aba67d06bd5ea5fd52b
JoseMRezende <rezendinhobr18@gmail.com>
2023-03-15 21:12:43 -0300

    Test branch Master 2

--- App.java
+++ App.java
@@ -3,6 +3,8 @@ package fp3.tech;
 public class App {
 
     public static void main(String[] args) {
-        
+        System.out.println("Teste");
+        System.out.println("Branch");
+        System.out.println("Master");
     }
 }
